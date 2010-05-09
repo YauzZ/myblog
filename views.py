0fe93a5b9179a9f298ab95d9aaae30a6fdae9976
dev <dev@YauzZ#2.(none)>
2010-05-09 16:17:45 +0800

    用render_to_response使用模板系统，可以实现将模板index.html和字典分开存放。

--- views.py
+++ views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.template import Context,Template
+from django.shortcuts import render_to_response
 
 def index(request):
-    t = Template('<html><body><p>Template <p> client ip: {{ ip }}</body></html>')
-    c = Context({'ip': request.META['REMOTE_ADDR']})
-    html = t.render(c)
-    return HttpResponse(html)
+#    t = Template('<html><body><p>Template <p> client ip: {{ ip }}</body></html>')
+
+    return render_to_response('index.html',{'ip': request.META['REMOTE_ADDR']})
