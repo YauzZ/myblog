59ff017d6d9ce176150a2fd693a0d897f8bc93f4
dev <dev@YauzZ#2.(none)>
2010-05-09 16:29:33 +0800

    使用模板标签和过滤器(类似与管道)

--- views.py
+++ views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render_to_response
 def index(request):
 #    t = Template('<html><body><p>Template <p> client ip: {{ ip }}</body></html>')
 
-    return render_to_response('index.html',{'ip': request.META['REMOTE_ADDR']})
+    return render_to_response('index.html',{'meta': request.META})
